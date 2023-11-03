bb195dae7a4df90e04fc2138a43891a8eac79065
Rohit Kumar <121628211+starkIncpr@users.noreply.github.com>
2023-11-03 08:37:30 +0530

    Add files via upload

---
+++ operationD7.js
@@ -0,0 +1,39 @@
+//********operation */
+
+let value = 5
+let negValue = -value
+//  console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+
+let str1 = "Rohit"
+let str2 = " Kumar"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" +2);
+// console.log(1+ "2");
+// console.log(1 + 2 + "2");
+
+// console.log((3+ 4) * 5 % 3);
+
+
+// console.log(+true);  // +true(1) true(true) true+(error) +""(1,0)
+
+
+// let num1,num2,num3
+
+// num1=num2=num3=2+2
+// console.log(num1);
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
